b7df245bbfba2910e640de9c916b67950ff54e47
Dalk <102332227+Dalk21@users.noreply.github.com>
2025-05-03 15:39:05 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# i2 LOT8s Music
+This repo contains 2 very simple scripts, hd_music and sd_music.\
+To use them, drag a audio file into the folder, run the script, and enter the filename when prompted.\
+Default duration is 65 for a 1950 duration LOT8s, but can be set to 120 for a 3600 duration Azul LOT8s.\
+Included is an example of the hd_music script using "Prelude to Presents" (2012, Lemon Demon).\
+There are also example flavor files for this. Place the file in "C:/Program Files/TWC/i2/Managed/Flavors/domestic(SD)" and run "domestic/htagEXAMPLE" (or whatever you have changed the filename to), and it will run. You need to change the path to the .ts file outputted from the script in the managed flavor .xml file.\
+There are also included modifications to I2-Heartbeat by [FortyFiveDegrees](https://github.com/FortyFiveDegrees/i2-heartbeat).\
+Made by TWCDalk with love.
---
+++ sd_music.bat
@@ -0,0 +1,23 @@
+@echo off
+echo Made by @zeds1175 (Dalk/TWCDalk/dalked). By using this script, you are abiding by the terms of the AGPL license attached to this toolkit.
+set /p song_name=Enter the song filename (ex. Really Cool Wig.mp3): 
+set /p duration=Enter duration in seconds (optional, default is 65): 
+
+if "%duration%"=="" set duration=65
+
+set "input_wav=%song_name%"
+for %%F in ("%song_name%") do set "clean_name=%%~nF"
+set "output_ts=OUT_LOT8_%clean_name%.ts"
+set "input_video=output_file.ts"
+set "input_image=LOT8_HD.png"
+set "logo_wav=LOT8_SonicLogo.wav"
+set "combined_wav=combined_audio.wav"
+
+ffmpeg -y -i "%logo_wav%" -i "%input_wav%" -filter_complex "[0:0][1:0]concat=n=2:v=0:a=1[outa]" -map "[outa]" "%combined_wav%"
+
+ffmpeg -y -i "%input_video%" -i "%combined_wav%" -i "%input_image%" -map 0:v -map 1:a -c:v mpeg2video -c:a ac3 -ac 6 -b:a 448k -filter_complex "[0:v]loop=loop=-1:size=1:start=0[v];[v][2:v]overlay=0:0,scale=720:480" -t %duration% -f mpegts -pix_fmt yuv420p "%output_ts%"
+
+del "%combined_wav%" >nul 2>&1
+
+echo Process completed!
+pause
